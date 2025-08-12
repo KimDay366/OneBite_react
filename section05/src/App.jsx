7b422e7b4d62710b6274d63edd1b7fedce104d16
kimDay <kimjs910522@gmail.com>
2025-08-12 13:58:16 +0900

    Create App.jsx

---
+++ section05/src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css';
+import Register from './components/Register';
+import Hooks from './components/Hooks';
+
+function App() {   
+
+  return <>
+    <Register />
+    <Hooks />
+  </>
+  
+}
+
+export default App
